de945e52597a596ccb59b84fb95f1b0cfd1a0c4f
nagesh <nsura6061@gmail.com>
2025-03-23 14:23:37 -0400

    commented welcome

--- src/main/java/com/javatechie/controller/CourseController.java
+++ src/main/java/com/javatechie/controller/CourseController.java
@@ -63,10 +63,10 @@ public class CourseController {
     }
 
 
-    @GetMapping("/welcome")
+   /* @GetMapping("/welcome")
     public String greetings() {
         return "Hello Techie , AWS CICD Example working fine !";
-    }
+    }*/
 
 
 }
